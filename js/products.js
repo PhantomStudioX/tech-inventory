4fe4cd096504f562505d43e207aa2fc90fd5e1dd
phantom.coded <michaelgraham565@gmail.com>
2025-11-28 15:06:30 -0500

    Add sample product inventory to products.js

--- js/products.js
+++ js/products.js
@@ -1,1 +1,35 @@
-
+// Sample inventory
+const products = [
+  {
+    id: 1,
+    name: "iPhone 15",
+    category: "phones",
+    price: "$999",
+    stock: 5,
+    image: "images/iphone15.jpg"
+  },
+  {
+    id: 2,
+    name: "Samsung Galaxy Tab S9",
+    category: "tablets",
+    price: "$699",
+    stock: 3,
+    image: "images/galaxy-tab-s9.jpg"
+  },
+  {
+    id: 3,
+    name: "MacBook Air M2",
+    category: "laptops",
+    price: "$1199",
+    stock: 2,
+    image: "images/macbook-air.jpg"
+  },
+  {
+    id: 4,
+    name: "Wireless Earbuds",
+    category: "accessories",
+    price: "$129",
+    stock: 10,
+    image: "images/earbuds.jpg"
+  }
+];
