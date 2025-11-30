27d03727df0486f2622bc979e661ca3aab5127db
phantom.coded <michaelgraham565@gmail.com>
2025-11-29 20:27:21 -0500

    Update stock quantity for iPhone 15

--- js/products.js
+++ js/products.js
@@ -9,7 +9,7 @@ const products = [
     name: "iPhone 15",
     category: "phones",
     price: "$999",
-    stock: 4,
+    stock: 99,
     image: "images/iphone15-black.jpg",
     colors: ["black", "blue"]
   },
